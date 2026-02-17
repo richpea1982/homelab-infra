8b82535ca6713fe7606e4676b57127970792d208
rich <rpearsall1982@gmail.com>
2026-02-18 00:42:25 +0100

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,20 @@
 terraform {
   required_version = ">= 1.6.0"
 
-  backend "s3" {
-    bucket                      = "terraform-state"
-    key                         = "homelab/router/terraform.tfstate"
-    region                      = "us-east-1"
-    endpoint                    = "http://docker-srv:9000"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    force_path_style            = true
+backend "s3" {
+    bucket   = "terraform-state"
+    key      = "homelab/router/terraform.tfstate"
+    region   = "us-east-1"
+
+    endpoints = {
+      s3 = "http://docker-srv:9000"
+    }
+
+    skip_credentials_validation  = true
+    skip_metadata_api_check      = true
+    skip_region_validation       = true
+    skip_requesting_account_id   = true
+    force_path_style             = true
   }
 
   required_providers {
