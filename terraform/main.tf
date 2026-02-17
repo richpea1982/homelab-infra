579a0bbee27d8aace3f4fe07a3e83d54ec0457a3
rich <rpearsall1982@gmail.com>
2026-02-18 00:44:03 +0100

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,11 +10,11 @@ backend "s3" {
       s3 = "http://docker-srv:9000"
     }
 
-    skip_credentials_validation  = true
-    skip_metadata_api_check      = true
-    skip_region_validation       = true
-    skip_requesting_account_id   = true
-    force_path_style             = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
   }
 
   required_providers {
