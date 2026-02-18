ef33df56216940551c342b5dd790322319c00024
rich <rpearsall1982@gmail.com>
2026-02-18 13:02:45 +0100

    modified terraforms provisioning for wp websites

--- terraform/modules/wp-lxc/main.tf
+++ terraform/modules/wp-lxc/main.tf
@@ -6,19 +6,21 @@ terraform {
     }
   }
 }
+
 resource "proxmox_virtual_environment_container" "wp_lxc" {
   node_name   = var.node
   vm_id       = var.vmid
   description = "WordPress LXC - ${var.hostname}"
+  started     = true
 
   initialization {
     hostname = var.hostname
 
-    network_interface {
-      name     = "eth0"
-      address  = "${var.ip}/24"
-      gateway  = var.gateway
-      vlan_id  = 20
+    ip_config {
+      ipv4 {
+        address = "${var.ip}/24"
+        gateway = var.gateway
+      }
     }
   }
 
@@ -49,6 +51,4 @@ resource "proxmox_virtual_environment_container" "wp_lxc" {
   features {
     nesting = true
   }
-
-  started = true
 }
